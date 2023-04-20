27a127a1f988c10ee349b76763f20047c85fe627
lalewi <Larry.Lewis@sas.com>
2023-04-20 15:06:39 -0400

    initial commit by lel

---
+++ ssg/site.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+
+class Site:
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(source)
+        directory.mkdir(parents=True, exists_ok=True)
+
