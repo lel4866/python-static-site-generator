006d4c0fb6b6374490531fa9dad2b8ef36ead72e
lalewi <Larry.Lewis@sas.com>
2023-04-20 15:13:58 -0400

    fix bug

--- ssg/site.py
+++ ssg/site.py
@@ -11,5 +11,5 @@ class Site:
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
 
-    def build():
+    def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
