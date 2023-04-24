ebbd462b0a657b522dd9ef58c70b11b15b4a84bd
lalewi <Larry.Lewis@sas.com>
2023-04-24 09:16:59 -0400

    added Parser class

---
+++ ssg/parsers.py
@@ -0,0 +1,16 @@
+from typing import List
+from pathlib import Path
+
+
+class Parser:
+    self.extensions: List[str] = []
+
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplemenetdError()
+
+
