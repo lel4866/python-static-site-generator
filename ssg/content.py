d8e68042ccf702a0b62ab418cd923a1663254291
lalewi <Larry.Lewis@sas.com>
2023-04-24 11:18:27 -0400

    fix bugs

--- ssg/content.py
+++ ssg/content.py
@@ -1,10 +1,11 @@
 import re
+
 from collections.abc import Mapping
 from yaml import load, FullLoader
 
 
 Class Content(Mapping):
-    __delimiter = "^(?:-|\+){3}\s*$"
+    __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
 
