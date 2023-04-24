beb0413fc88d77021d8cafaa4a6417441c16912e
lalewi <Larry.Lewis@sas.com>
2023-04-24 10:50:17 -0400

    fix bugs

--- ssg/site.py
+++ ssg/site.py
@@ -22,13 +22,13 @@ class Site:
                 self.run_parser(path)
 
 
-    def load_parser(extension):
+    def load_parser(self, extension):
         for parser in self.parsers:
             if parser.valid_extension(extension):
                 return parser
 
 
-    def run_parser(path):
+    def run_parser(self, path):
         parser = self.load_parser(path.suffix)
         if parser is not None:
             parser.parse(path, self.source, self.dest)
