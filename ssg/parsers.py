8456af9da676dba84f3dcdcb07fae9930e4b7bc9
lalewi <Larry.Lewis@sas.com>
2023-04-24 14:37:36 -0400

    add ReStructuredText parser

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,7 +1,13 @@
 import shutil
+import sys
 
 from typing import List
 from pathlib import Path
+
+from docutils.core import publish_parts
+from markdown import markdown
+
+from ssg.content import Content
 
 
 class Parser:
@@ -37,5 +43,23 @@ class ResourceParser(Parser):
     
     def parse(self, path: Path, source: Path, dest: Path):
         copy(path, source, dest)
+
+
+class MarkdownParser(Parser):
+    extensions = [".md", ".markdown"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+
+
+class ReStructuredTextParser(Parser):
+    extensions = [".rst"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
+        publish_parts(content.body, writer_name="html5")
 
 
