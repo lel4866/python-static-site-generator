7885185f7a4347a4b82bfea75c993dfc62567dad
lalewi <Larry.Lewis@sas.com>
2023-04-25 10:46:18 -0400

    fix bug

--- ssg.py
+++ ssg.py
@@ -1,10 +1,16 @@
-import ssg.parsers
 import typer
 from ssg.site import Site
 
+import ssg.parsers
+
 
 def main(source="content", dest="dist"):
-    config={"source": source, "dest": dest, "parsers": [ssg.parsers.ResourceParser()]}
+    config={"source": source, "dest": dest, "parsers": [
+        ssg.parsers.ResourceParser()
+        ssg.parsers.MarkdownParser(),
+        ssg.parsers.ReStructuredTextParser(),
+        ],
+    }
     site = Site(**config).build()
 
 
